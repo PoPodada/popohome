1f96a895a4e62e3f48b7a8f16d8fc661b7b78512
go <goito.722@gmail.com>
2023-09-19 10:16:51 +0900

    node-express

---
+++ popohome/node-express/server.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const PORT=3000;
+const userRouter=require("./routes/user");
+
+// app.use(mylogger);
+// app.use(express.static("public"));
+app.set("view engine","ejs");
+
+
+app.get("/",(req,res)=>{
+    // console.log("hello express");
+    // res.send("<h1>こんにちは</h1>");
+    // res.sendStatus(404);
+    // res.status(500).send("エラーです");
+    // res.status(500).json({msg:"エラーです"});
+    res.render("index",{text:"nodejsとexpress"});
+});
+
+
+app.use("/user",userRouter);
+// app.use("/auth",authRouter);
+// app.use("/customer",customerRouter);
+// app.use("/product",productRouter); 
+
+
+
+
+app.listen(PORT,()=>console.log("サーバーが起動しました．"));
