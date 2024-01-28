a6518b6bed9f4c47a9edd5133675d1665f58eafa
go <goito.722@icloud.com>
2024-01-28 21:59:56 +0900

    testtest

---
+++ js-practice/index.js
@@ -0,0 +1,53 @@
+// const name = "じゃけえ";
+// const age = 31;
+// const message2 = `私の名前は${name}です年齢は${age}です`;
+// console.log(message2);
+// const func2 =str=>str
+
+// console.log(func2("aiueo"));
+
+
+// const func = (num1,num2)=> ({
+//     hoge:num1,
+//     fuga:num2,
+// });
+// console.log(func(1,2));
+
+// const myProfile = {
+//     name:"じゃけえ",
+//     age:21
+// }
+// // const message1 = `名前は${myProfile.name}です年齢${myProfile.age}`;
+// // console.log(message1);
+
+// const {name,age} = myProfile;
+// const message2 = `名前は${name}です年齢${age}`
+// console.log(message2)
+
+// const myProfile = ["じゃけえ",31];
+// const message3 = `名前は${myProfile[0]}${myProfile[1]}`
+
+// const [name,age] = myProfile;
+// console.log(name,age)
+// const sayhello = (name = "tintin") => console.log(`こんにちは${name}さん`);
+// sayhello(";lfahjflk@pdsahj")
+
+// const myprofile = {
+//     age:31,
+//     name:"fjlasjfdslsakjf;lasdkjf;lsadkjf;lsadkjf;laskfj"
+// }
+
+// const {age,name ="gest"} = myprofile;
+
+// console.log(age,name)
+
+// const name = "じゃけえ";
+// const age = 31;
+// const myprofile = {
+//     name,
+//     age
+// }
+// console.log(myprofile)
+
+const arr1 = [1,2];
+console.log(...arr1);
